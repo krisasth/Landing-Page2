a4bdb0b6dec6d999b261158dfff4bbb4730d8cd5
yato <shreejann18@gmail.com>
2025-04-11 09:20:48 +0545

    header

--- header.php
+++ header.php
@@ -11,4 +11,31 @@
     <title>All Clear</title>
 </head>
 <body>
+    <header class="site-header">
+            <div class="container">
+                <div class="content">
+                    <div class="header-logo">
+                        <figure>
+                            <img src="./assets/images/logo.png" alt="Header Logo" width="142px" height="127px" loading="lazy">
+                        </figure>
+                    </div>
+                    <div class="header-buttons">
+                        <a href="mailto:info@allclearplumbing.org">
+                            <i class="icon-email"></i>
+                            <span class="a-text">Email</span>
+                            <span class="a-text2">info@allclearplumbing.org</span>
+                        </a> 
+                        <a href="tel:8187362616">
+                            <i class="icon-phonenumber"></i>
+                            <span class="a-text">Phone Number</span>
+                            <span class="a-text2">(818) 736-2616</span>
+                        </a>
+                    </div>
+                        <div>
+                            <a href="#" class="btn">Call Us Today</a>
+                        </div>
+                </div>
+            </div>
+        </header>
+
  
